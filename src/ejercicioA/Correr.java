bf72de1c48691e5683d0edd5c9b32f7172a2ecb1
Hector Cuesta <kuestatv@gmal.com>
2015-10-30 14:03:20 +0100

    Before comment exercise A

--- src/ejercicioA/Correr.java
+++ src/ejercicioA/Correr.java
@@ -1,7 +1,47 @@
 package ejercicioA;
 
-/**
- * Created by H3ku on 30/10/15.
- */
-public class Correr {
+class Puerta {
+    public static boolean abierta;
+    public static int contador;
 }
+class Abrir extends Thread {
+    public void run() {
+        for (int i = 0; i < 1000; i++)
+            if (!Puerta.abierta)
+                Puerta.abierta = true;
+            else {
+                i--;
+                Puerta.contador++;
+            }
+        System.out.println("Abrir terminando");
+    }
+}
+
+
+class Cerrar extends Thread {
+    public void run() {
+        for (int i = 0; i < 1000; i++)
+            if (Puerta.abierta)
+                Puerta.abierta = false;
+            else {
+                i--;
+                Puerta.contador++;
+            }
+        System.out.println("Cerrar terminando");
+    }
+}
+
+public class Correr {
+    public static void main(String[] args) throws InterruptedException {
+        Puerta.abierta = true;
+        Thread a = new Abrir();
+        Thread c = new Cerrar();
+        a.start();
+        c.start();
+        a.join();
+        c.join();
+        System.out.println("El resultado final es: " + Puerta.contador);
+    }
+
+}
+
