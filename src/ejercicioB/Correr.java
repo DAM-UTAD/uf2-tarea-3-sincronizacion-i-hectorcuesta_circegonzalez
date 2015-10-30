95e0a731e88c718d747ba9b8acd61eb5405eef78
circe.gonzalez <circegonzarey@gmail.com>
2015-10-30 14:37:47 +0100

    ejercicio B (parte 1)comentado

--- src/ejercicioB/Correr.java
+++ src/ejercicioB/Correr.java
@@ -24,11 +24,16 @@ class Puerta {
  *
  */
 class LlaveA extends Thread {
+	/**
+	 * método que ejecuta el hilo
+	 */
 	public void run() {
-		while (!Puerta.CerrojoB)
-			;
+		while (!Puerta.CerrojoB);
+		//si el cerrojo es distinto de B (es decir, es A)
 		Puerta.CerrojoA = true;
+		//abrir el cerrojo A
 		System.out.println("LlaveA terminando");
+		
 	}
 }
 
@@ -40,27 +45,42 @@ class LlaveA extends Thread {
 class LlaveB extends Thread {
 	public void run() {
 		while (!Puerta.CerrojoA)
+			//si el cerrojo es distinto de A (es decir, es B)
 			;
 		Puerta.CerrojoB = true;
+		//abrir cerrojo B
 		System.out.println("LlaveB terminando");
 	}
 }
 
 /**
  * Created by H3ku on 30/10/15.
+ * hilo principal de ejecución 
  */
 public class Correr {
 	public static void main(String[] args) throws InterruptedException {
 		Puerta.CerrojoA = false;
+		//cerrar el cerrojo A
 		Puerta.CerrojoB = false;
+		//cerrar el cerrojo B
 		Thread a = new LlaveA();
+		//crear el hilo de la llave A
 		Thread b = new LlaveB();
+		//crear el hilo de la llave B
 		a.start();
+		//iniciar el hilo de la llave A
 		b.start();
+		//iniciar el hilo de la llave B
 		System.out.println("Comienzo del hilo principal");
 		a.join();
+		//el hilo llave A debe esperar a que termine el hilo llave B
 		b.join();
+		//el hiilo llave B debe esperar a que termine el hilo llave A
 		System.out.println("Fin del hilo principal");
 	}
-
+/*
+ * el programa no funciona porque se produce una situación de interbloqueo
+ * ya que ambos hilos estan esperándose mutuamente a que el otro termine su ejecución
+ */
 }
+
