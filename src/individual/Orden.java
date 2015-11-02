35085f7f50edb2d371d65e1bfd5b219cf664b10c
Hector Cuesta <kuestatv@gmail.com>
2015-11-02 23:04:40 +0100

    Added semaphore to sync our threads.

--- src/individual/Orden.java
+++ src/individual/Orden.java
@@ -1,15 +1,28 @@
 package individual;
 
+import java.util.concurrent.Semaphore;
+
 public class Orden{
     public static void main(String[] args) {
 
+        //Declaramos el semaforo dandole un valor incial de 0.
+        Semaphore letterFinish = new Semaphore(0);
+
+        //Nuestro primer thread imprimira letras, cuando termine incrementara el valor del semaforo.
         Runnable letters = () -> {
             for (int i = 'a'; i <= 'j'; i++) {
                 System.out.println(Character.toChars(i));
             }
+
+            letterFinish.release();
         };
 
+
+        //Nuestro segundo thread intentara restar uno al semaforo, como es cero no podra hasta que se terminen de
+        //Imprimir las letras y se incremente en uno.
         Runnable numbers = () -> {
+            letterFinish.acquireUninterruptibly();
+
             for (int i = 1; i <= 10; i++) {
                 System.out.println(i);
             }
@@ -18,10 +31,6 @@ public class Orden{
         //Instanciamos los dos Threads.
         Thread t1 = new Thread(letters);
         Thread t2 = new Thread(numbers);
-
-        //Definimos sus prioridades.
-        t1.setPriority(Thread.MAX_PRIORITY);
-        t2.setPriority(Thread.MIN_PRIORITY);
 
         //Iniciamos los threads.
         t1.start();
