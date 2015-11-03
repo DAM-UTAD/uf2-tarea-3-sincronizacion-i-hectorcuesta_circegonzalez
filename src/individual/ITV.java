7926733a98d1ec09ef30a6a2e280b180859c4c01
Hector Cuesta <kuestatv@gmail.com>
2015-11-03 20:53:36 +0100

    Modify version to use individual thread for cars and mechanics.

--- src/individual/ITV.java
+++ src/individual/ITV.java
@@ -1,5 +1,6 @@
 package individual;
 
+import java.util.LinkedList;
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeUnit;
@@ -7,43 +8,87 @@ import java.util.concurrent.TimeUnit;
 /**
  * Created by Hector Cuesta on 03/11/2015.
  */
-public class ITV {
 
+class GlobalSettings{
+    //Creamos las variables puestos y coches asignandoles un valor random comprendido entre los valores indicados.
+    static int workers = ThreadLocalRandom.current().nextInt(1, 5 + 1);
+    static int cars = ThreadLocalRandom.current().nextInt(20, 50 + 1);
 
-    public static void main(String[] args) {
-        //Creamos las variables puestos y coches asignandoles un valor random comprendido entre los valores indicados.
-        int workers = ThreadLocalRandom.current().nextInt(1, 5 + 1);
-        int cars = ThreadLocalRandom.current().nextInt(20, 50 + 1);
+    //Creamos un semaforo con el numero de trabajadores.
+    static Semaphore usingCarsArray = new Semaphore(1);
 
-        //Creamos un Array para almacenar llos threads.
-        Thread[] threads = new Thread[cars];
+    //Creamos un Array para almacenar los threads.
+    static LinkedList<Thread> carThreads = new LinkedList<>();
 
-        //Creamos un semaforo con el numero de trabajadores.
-        Semaphore maxWorkers = new Semaphore(workers);
+    //Creamos un Array para almacenar los threads.
+    static Thread[] workerThreads = new Thread[workers];
+}
 
-        System.out.println("Hay "+cars+" vehiculos que seran atendidos en "+workers+" puestos de inspeccion");
+class CarThread implements Runnable {
+    @Override
+    public void run() {
+        long tiempoRevision = ThreadLocalRandom.current().nextLong(10, 100 + 1); //Calculamos el tiempo de revision.
+        try {
+            TimeUnit.MILLISECONDS.sleep(tiempoRevision); //Esperamos los milisegundos de la revision.
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
 
-        Runnable revision = () -> {
-            long tiempoRevision = ThreadLocalRandom.current().nextLong(10, 100 + 1); //Calculamos el tiempo de revision.
+class WorkerThread implements Runnable {
+
+    int workerName;
+    String carName;
+
+    public WorkerThread(int pWorkerName){
+        workerName = pWorkerName;
+    }
+    @Override
+    public void run() {
+
+        //Mientras existan coches repetimos.
+        while (GlobalSettings.carThreads.size() > 0){
+            Thread revision;
+
+            //Indicamos que estamos usando el array de coches para evitar RaceConditions.
+            GlobalSettings.usingCarsArray.acquireUninterruptibly();
+            revision = GlobalSettings.carThreads.pop();
+            carName = revision.getName();
+            revision.start();
+            GlobalSettings.usingCarsArray.release(); //Liberamos el uso de el Array.
             try {
-                maxWorkers.acquireUninterruptibly(); //Restamos un trabajador.
-                TimeUnit.MILLISECONDS.sleep(tiempoRevision); //Esperamos los milisegundos de la revision.
-                maxWorkers.release(); //Sumamos un trabajador.
+                revision.join();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-        };
 
-        //Inicializamos los thread.
-        for (int i = 0; i < cars ; i++) {
-            threads[i] = new Thread(revision);
-            threads[i].start();
+            System.out.println("El puesto numero " + workerName + " ha atendido el vehiculo "+ carName);
+        }
+    }
+}
+
+public class ITV {
+    public static void main(String[] args) {
+
+        System.out.println("Hay "+GlobalSettings.cars+" vehiculos que seran atendidos en "+GlobalSettings.workers+" puestos de inspeccion");
+
+        //Inicializamos los thread coche.
+        for (int i = 0; i < GlobalSettings.cars ; i++) {
+            Thread car =  new Thread(new CarThread());
+            car.setName(String.valueOf(i+1));
+            GlobalSettings.carThreads.add(car);
         }
 
-        //Usamos Join para esperar que terminen.
-        for(Thread thread : threads){
+        //Inicializamos los thread de los trabajadores.
+        for (int i = 0; i < GlobalSettings.workers ; i++) {
+            GlobalSettings.workerThreads[i] = new Thread(new WorkerThread(i+1));
+            GlobalSettings.workerThreads[i].start();
+        }
+
+        for (Thread worker : GlobalSettings.workerThreads){
             try {
-                thread.join();
+                worker.join();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
